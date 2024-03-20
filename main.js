ed402823ce27e6cbc6274a2e6a3b98965470898a
seuncoded <seuncodedmedia@gmail.com>
2024-03-20 13:45:08 +0100

    Makes Lots of Changes

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
+let todoarr = []
 
-function addTodoList(){
-    if(inputBox.value == ''){
-        alert('You Must Write Something')
-    }else{
-        let li = document.createElement('li')
-        li.innerHTML = inputBox.value 
-        listContainer.append(li)
 
-        console.log(inputBox.value);
-        console.log(li);
+
+
+function dispList(){
+    listContainer.innerHTML = "";
+    for (let i = 0; i < todoarr.length; i++) {
+        listContainer.innerHTML += `<li>${todoarr[i]} 
+        <span><button id="btn1" onclick=delItem(${i})>Delete</button>
+        <button id="btn2" onclick=editItem(${i})>Edit</button></span></li>`
+
     }
-    inputBox.value = "";
+}
+
+
+function addTodoList() {
+    if (inputBox.value == '') {
+        alert('You Must Write Something')
+    } else {
+        todoarr.unshift(inputBox.value);
+        dispList()
+
+    }
+    inputBox.innerHTML = "";
+}
+
+
+function delItem(p){
+    todoarr.splice(p, 1);
+    dispList()
+}
+
+
+function editItem(r){
+    let changing = prompt('Enter The New List');
+    todoarr.splice(r, 1, changing);
+    dispList()
 }
